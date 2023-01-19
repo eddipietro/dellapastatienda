5a50a0e509e2941b8e929095c81c9c9e71000a08
eddipietro <94018665+eddipietro@users.noreply.github.com>
2023-01-19 17:59:35 -0300

    actualizacion Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyCWPGdmXbaHkGUKTY9DRxpoMq24J2MdlyA",
   authDomain: "dellapastatienda-d2349.firebaseapp.com",
   projectId: "dellapastatienda-d2349",
   storageBucket: "dellapastatienda-d2349.appspot.com",
   messagingSenderId: "430945681243",
-  appId: "1:430945681243:web:8d11d8fb3523c6c281647e"
+  appId: "1:430945681243:web:8d11d8fb3523c6c281647e",
+  rules_version : '2'
+
 };
 
 
